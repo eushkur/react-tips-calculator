ff9c260a939e9e75565dfcb129011491d7628379
EvgenyUshkur <110987191+eushkur@users.noreply.github.com>
2023-03-06 13:21:51 +0300

    add reat-tips-calculator

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyledApp } from './styles';
+import { Form } from './components/Form/Form';
+
+export const App = () => {
+  return (
+    <StyledApp>
+      <Form />
+    </StyledApp>
+  );
+};
